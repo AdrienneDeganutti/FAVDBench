249a24077b4d48a48bcb15277ba65c8944e6558e
Adrienne Deganutti <ad01355@surrey.ac.uk>
2024-01-05 21:59:58 +0000

    config changes

--- AVLFormer/inference.sh
+++ AVLFormer/inference.sh
@@ -1,7 +1,7 @@
 python \
     src/tasks/inference.py \
     --eval_model_dir output/favd_default \
-    --test_video_fname datasets/videos/test \
-    --test_audio_fname datasets/audio_mp3/test \
+    --test_video_fname mnt/welles/scratch/adrienne/FAVDBench/videos/test \
+    --test_audio_fname mnt/welles/scratch/adrienne/FAVDBench/audio_mp3/test \
     --do_lower_case \
     --do_test \
